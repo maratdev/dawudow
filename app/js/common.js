a77d1ab6df8d69f34fcdd2b61dfde37f4810cfe2
TheDonKing <starspersona@gmail.com>
2016-09-22 17:53:02 +0300

    Добавления footera

--- app/js/common.js
+++ app/js/common.js
@@ -5,9 +5,6 @@ $(function() {
         $("header").css("height", $(window).height());
         $(".wrapper-home_center").css("height", $(window).height());
         $(".it1").css("height", $(window).height());
-        $(".it2").css("height", $(window).height());
-        $(".it3").css("height", $(window).height());
-        $(".it4").css("height", $(window).height());
     }
 
     heightDetect();
@@ -16,27 +13,29 @@ $(function() {
     });
 
 
+
     //Каруселька Slider
     var sync1 = $(".sync1");
     var sync2 = $(".sync2");
 
     sync1.owlCarousel({
-        items:1,
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:1000,
-        autoplayHoverPause:true,
-        animateInClass: 'fadeIn'
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        singleItem : true,
+        navigation: true,
+        pagination:false,
+        afterAction : syncPosition,
+        responsiveRefreshRate : 200,
+        beforeMove : function(el){
+        }
     });
 
     sync2.owlCarousel({
-        animateInClass: 'fadeIn',
         items : 8,
-        mouseDrag: false,
-        touchDrag: false,
-        pullDrag: false,
-        autoWidth: true,
+        itemsDesktop      : [1199,10],
+        itemsDesktopSmall     : [979,10],
+        itemsTablet       : [768,8],
+        itemsMobile       : [479,4],
         pagination:false,
         responsiveRefreshRate : 100,
         afterInit : function(el){
@@ -62,24 +61,14 @@ $(function() {
         sync1.trigger("owl.goTo",number);
     });
 
-    var wow = new WOW({
-        boxClass:     'wow',      // animated element css class (default is wow)
-        animateClass: 'animated', // animation css class (default is animated)
-        offset:       0,          // distance to the element when triggering the animation (default is 0)
-        mobile:       true,       // trigger animations on mobile devices (default is true)
-        live:         true,       // act on asynchronously loaded content (default is true)
-        callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-        }
-    });
-    wow.init();
+
 
 
 
     //$(".loader").fadeOut();
     //$(".loader_inner").delay(800).fadeOut("slow");
-    //$("html").niceScroll();
+
+    $("html").niceScroll();
 
 });
 
