7b94a05fe5ab031cc96f5b1ab24355fd9c3c939b
TheDonKing <starspersona@gmail.com>
2016-12-03 19:07:30 +0300

    Добавление gif анимации

--- app/js/common.js
+++ app/js/common.js
@@ -136,7 +136,7 @@ $(function() {
     $('.scene').parallax();
 
    $(".loader").fadeOut();
-   $(".loader_inner").delay(2000).fadeOut("slow");
+   $(".loader_inner").delay(3000).fadeOut("slow");
    $(".sync2 .owl-item").attr('style', '');
    $(".slides_control").attr('style', '');
 
